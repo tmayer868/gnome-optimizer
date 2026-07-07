eec84e74f4aaea377ae80c26b7dc5efe46922299
tony <tmayer868@gmail.com>
2026-07-07 11:05:51 -0600

    Built site for gh-pages

--- experiments/baselines/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Baselines for Gnome benchmarks.
-
-Currently the SOAP optimizer (https://arxiv.org/abs/2409.11321). AdamW comes
-from ``torch.optim`` and is not re-exported.
-"""
-
-from experiments.baselines.soap import SOAP
-
-__all__ = ["SOAP"]
--- gnome/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Gnome: Gauss-Newton Optimizer via Matrix Eigen-decomposition."""
-
-from gnome.blocks import stack_residuals
-from gnome.optimizer import Gnome
-
-__all__ = ["Gnome", "stack_residuals"]
-__version__ = "0.1.0"
